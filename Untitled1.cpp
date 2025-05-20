683d4a909fad69462365c61c2d78b7f63a19f75e
krish <jistha805gmail.com>
2025-05-20 10:47:37 -0700

    basics done

---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void display();
+int main()
+{
+	display();
+	return 0;
+{
+}
+	void display();
+}
+cout<<'hello';
---
+++ Untitled2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+ using  namespace std;
+ 
+ void display()
+ {
+ 	cout<<"Hello"<<end1;
+ }
+ void display (string msg)
+ {
+ 	cout<<msg,<<nd1;
+ 	
+ }
+ void display(int a)
+ {
+ 	cout<<a<<end1;
+ } 
+ 
+   void display(double)
+   {
+   	cout<<a<<end1;
+   }
